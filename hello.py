d3435928d3d55beeeec1f7188639c1c7f03c149f
dcb9bs <Drew@d-172-25-44-156.bootp.Virginia.EDU>
2015-09-28 14:13:09 -0400

    helper.py was created and now holds the greeting function

--- hello.py
+++ hello.py
@@ -1,7 +1,6 @@
 __author__ = 'dcb9bs'
 
-def greeting(msg):
-    print(msg)
+import helper
 
 if __name__ == "__main__":
-    greeting("hello")
+    helper.greeting("hello")
---
+++ helper.py
@@ -0,0 +1,4 @@
+__author__ = 'dcb9bs'
+
+def greeting(msg):
+    print(msg)
